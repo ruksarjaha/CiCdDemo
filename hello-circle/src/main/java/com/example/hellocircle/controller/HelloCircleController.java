ec098bc6433bb6129da430a83f60e5d8698a21cb
Eshan Mohammad <eshan@Eshans-MacBook-Air.local>
2022-07-28 12:31:31 -0700

    first commit

---
+++ hello-circle/src/main/java/com/example/hellocircle/controller/HelloCircleController.java
@@ -0,0 +1,13 @@
+package com.example.hellocircle.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloCircleController {
+
+    @GetMapping(value = "/hello")
+    public String helloCircle(){
+        return "Hello, Circle!";
+    }
+}
